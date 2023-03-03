8459b4475f129203a2c29e8a519d8115550e25ff
oscar <oscar1322134219@uptecamac.edu.mx>
2023-03-02 18:35:13 -0600

    m

---
+++ js/helper.js
@@ -0,0 +1,34 @@
+// // Name and Password from the register-form
+// var name = document.getElementById('name');
+// var pw = document.getElementById('pw');
+
+// // storing input from register-form
+// function store() {
+//     localStorage.setItem('name', name.value);
+//     localStorage.setItem('pw', pw.value);
+// }
+
+// // check if stored data from register-form is equal to entered data in the   login-form
+// function check() {
+
+//     // stored data from the register-form
+//     var storedName = localStorage.getItem('name');
+//     var storedPw = localStorage.getItem('pw');
+
+//     // entered data from the login-form
+//     var userName = document.getElementById('userName');
+//     var userPw = document.getElementById('userPw');
+
+//     // check if stored data from register-form is equal to data from login form
+//     if(userName.value !== storedName || userPw.value !== storedPw) {
+//         alert('ERROR');
+//     }else {
+//         alert('You are loged in.');
+//     }
+//
+
+function sacarlocaldatos (){
+    let datillos = localStorage.getItem("Datos-Alumnos")
+    datillos=JSON.parse(datillos);
+    console.log(datillos);
+}
--- js/main.js
+++ js/main.js
@@ -25,30 +25,14 @@ function guardarDatosInscripcion() {
  matricula_gen.innerHTML = matricula;
 
  /*se guarda el alumno en local */
+
   let alum07 = new Alumno(nombre, apellido_pat,apellido_mat, nss, fecha,promedio_bachillerato,correo,contraseña,matricula);
   alumnos.push(alum07);
-  localStorage.setItem("Datos-Alumnos", JSON.stringify(alumnos));
+localStorage.setItem("Datos-Alumnos", JSON.stringify(alumnos));
+console.log("Datos guardados en localStorage:", localStorage.getItem("Datos-Alumnos"));
+
   alert('Datos guardados correctamente');
-  var datos_almns = localStorage.getItem("Datos-Alumnos");
-  console.log(JSON.parse(datos_almns));
 }
-
-
-
-
-
-
-
-function generarMatricula() {
-
-  let nombre_ingresado = document.getElementById("nombre").value
-}
-
-
-
-
-
-
 
 /******************************************************************** */
 
@@ -56,64 +40,17 @@ function generarMatricula() {
 
 //pagina reinscripcion alumno
 
-function guardarDatosreinsc() {
-  alert("datos generados ajua")
-
+function Login() {
   let matriculas = document.getElementById("matricula").value;
   let grupos = document.getElementById("password").value;
 
-  var datillos = {
-    matricula: matricula, contraseña: contraseña, grupo: grupo
-  }
-
-  localStorage.setItem("datos-guardar", JSON.stringify(datillos));
-  alert('Datos guardados correctamente');
-
-  var datooos = localStorage.getItem("datos-guardar");
-  console.log(JSON.parse(datooos));
-
-  document.getElementById("Cambiar-btn").removeAttribute("disabled", "");
-
-  let result = alumnos.find(({ nombre }) => nombre === nombre.value);
-
-  if (result == undefined) {
-    users.push(user1);
-    localStorage.setItem('users', JSON.stringify(users));
-    alert("Usuario Registrado");
-    console.log(result);
-  }
-  
+  sacarlocaldatos();
   }
 
 
 
 /*************PAGINA DONDE SE SCOGEN MATERIAS */
 
-function materiasbtn() {
-  var materiasDiv = document.getElementById("materias");
-  if (materiasDiv.style.display === "none") {
-    materiasDiv.style.display = "block";
-  } else {
-    materiasDiv.style.display = "none";
-  }
-
-
-}
-
-
-
-var miCheckbox = document.getElementById("miCheckbox");
-
-if (miCheckbox) {
-  // Verificar si la casilla está seleccionada
-  if (miCheckbox.checked) {
-    console.log("La casilla está seleccionada");
-  } else {
-    console.log("La casilla no está seleccionada");
-  }
-} else {
-  console.log("No se encontró el elemento input");
-}
 
 
 
