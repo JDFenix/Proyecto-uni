d2b7f4c52922fd5a8d4f3761d78e4dc5d7303cbf
oscar <oscar1322134219@uptecamac.edu.mx>
2023-03-07 02:27:18 -0600

    Login admin

--- js/admin.js
+++ js/admin.js
@@ -1,20 +1,11 @@
-class administrador {
-    clave_issemin;
+class Administrador {
+    correo_admin;
     contraseña
-    constructor(contraseña, clave_issemin) {
-        this.contraseña;
-        this.clave_issemin;
+    
+    constructor(  correo_admin, contraseña) {
+        this.contraseña=contraseña;
+        this. correo_admin= correo_admin;
     }
 
 }
-const alumn004 = new Alumno("saul","mata","alegria","14255545712","18/09/2004","9.8", "saul@gmail.com","saul01","133258581","calculo");
-console.log(alumn004);
 
-
-function leer_alumnos() {
-    let alumnos = (localStorage.getItem("Datos-Alumnos"));
-    let datillos = document.getElementById("insertaralumnos");
-    console.log(alumnos);
-    datillos.innerHTML = alumn004;
-
-}
--- js/main.js
+++ js/main.js
@@ -12,26 +12,45 @@ function generar_materia() {
   let matricula_gen = document.getElementById("matricula-gen");
   var daletra = nombre.substring(0, 2).toUpperCase();
   var matriculag = daletra + "-" + Math.floor(Math.random() * 1000000);
-  matricula=matriculag;
+  matricula = matriculag;
   matricula_gen.innerHTML = matriculag;
 
-  
+
 }
 
 function guardarDatosInscripcion() {
-  var nombre = document.getElementById("nombre").value;
-  let apellido_pat = document.getElementById("ap_paterno").value;
-  let apellido_mat = document.getElementById("ap_materno").value;
-  let promedio_bachillerato = document.getElementById("promedio").value;
-  let nss = document.getElementById("nss").value;
-  let fecha = document.getElementById("fecha").value;
-  let correo = document.getElementById("correo").value
-  let contraseña = document.getElementById("contraseña").value;
+  var nombre = document.querySelector("#nombre").value;
+  var ap_paterno = document.querySelector("#ap_paterno").value;
+  var ap_materno = document.querySelector("#ap_materno").value;
+  var nss = document.querySelector("#nss").value;
+  var fecha = document.querySelector("#fecha").value;
+  var promedio = document.querySelector("#promedio").value;
+  var correo = document.querySelector("#correo").value;
+  var contraseña = document.querySelector("#contraseña").value;
 
- 
+  if (nombre.trim() === '' ||
+    ap_paterno.trim() === '' ||
+    ap_materno.trim() === '' ||
+    nss.trim() === '' ||
+    fecha.trim() === '' ||
+    promedio.trim() === '' ||
+    correo.trim() === '' ||
+    contraseña.trim() === '') {
+
+    Swal.fire({
+      icon: 'error',
+      title: 'ERROR',
+      text: 'FALTA LLENAR CAMPOS',
+      footer: 'Por favor llena todos los campos e intenta de nuevo'
+    });
+    return;
+  }
+
+
+
   generar_materia();
 
-  let alum07 = new Alumno(nombre, apellido_pat, apellido_mat, nss, fecha, promedio_bachillerato, correo, contraseña, matricula); // pasar la matrícula generada como parámetro
+  let alum07 = new Alumno(nombre, apellido_pat, apellido_mat, nss, fecha, promedio_bachillerato, correo, contraseña, matricula);
   alumnos.push(alum07);
 
   guardar_alumnos();
@@ -44,8 +63,8 @@ function guardar_alumnos() {
   let nss = document.getElementById("nss").value;
 
   let dats_alms = JSON.parse(localStorage.getItem("Datos-Alumnos")) || [];
-  
-  let verificacion_name = dats_alms.find(({ apellido_pat: ap, apellido_mat: am, correo: c ,nss:ns}) => ap === apellido_pat && am === apellido_mat && c === correo && ns === nss);
+
+  let verificacion_name = dats_alms.find(({ apellido_pat: ap, apellido_mat: am, correo: c, nss: ns }) => ap === apellido_pat && am === apellido_mat && c === correo && ns === nss);
 
   if (verificacion_name === undefined) {
     // El alumno no está duplicado en el localStorage
